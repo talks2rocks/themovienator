5b48a319d17e214b27c7a1ed83c51cf41ef6a124
Talks2Rocks <perlman.yoel@gmail.com>
2014-02-13 18:08:33 +0200

    Reorganize javascripts and css for different parts of the site

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,9 @@
 // require jquery_ujs
 // require turbolinks
 // require_tree .
-//= require modernizr
-//= require jquery-1.10.2
-//= require jquery-ui-1.10.4.custom.min.js
-//= require scripts
+//= require landing/modernizr.js
+//= require landing/jquery-1.10.2.js
+//= require landing/jquery-ui-1.10.4.custom.min.js
+//= require landing/scripts.js
 
 
---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,4 @@
+//= require landing/modernizr.js
+//= require landing/jquery-1.10.2.js
+//= require landing/jquery-ui-1.10.4.custom.min.js
+//= require landing/scripts.js
