5834b72908032ccac6f0c73b33423ecd4b264554
Talks2Rocks <perlman.yoel@gmail.com>
2014-02-14 01:06:27 +0200

    Get font working in production

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Themovienator
     #Add font to assets path
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
+      if path =~ /\.(css|js|otf|ttf)\z/
         full_path = Rails.application.assets.resolve(path).to_path
         app_assets_path = Rails.root.join('app', 'assets').to_path
         if full_path.starts_with? app_assets_path
