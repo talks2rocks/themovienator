13618a3956f2b7ad9ddf69b92a2db139fc1ad484
Talks2Rocks <perlman.yoel@gmail.com>
2014-02-14 18:26:21 +0200

    Create search form which can pass variables to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,8 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def search
+    @SearchInput = params[:SearchInput]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Themovienator::Application.routes.draw do
 
   resources :movies
   match "/addmovie", to: "movies#new", via: 'get'
+  match "/search", to: "movies#search", via: 'post'
   root :to => "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
